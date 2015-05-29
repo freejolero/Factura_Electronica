9e0f24ebfecc3469168f6489c8460709bd68956f
christian <christian@Chris-Pc>
2015-05-28 22:51:36 -0500

    Probando resolucion de prefijos

--- CompilarSchema/src/test/java/com/olva/sunatfe/test/bc/FacturaElectronicaTest.java
+++ CompilarSchema/src/test/java/com/olva/sunatfe/test/bc/FacturaElectronicaTest.java
@@ -5,29 +5,19 @@
  */
 package com.olva.sunatfe.test.bc;
 
-import com.olva.sunatfe.be.AdditionalInformationTypeSunatAgg;
-import com.olva.sunatfe.be.AdditionalMonetaryTotalType;
-import com.olva.sunatfe.be.ExtensionContentType;
-import com.olva.sunatfe.be.IDType;
-import com.olva.sunatfe.be.InvoiceType;
 import com.olva.sunatfe.be.ObjectFactory;
-import com.olva.sunatfe.be.PayableAmountType;
-import com.olva.sunatfe.be.UBLExtensionType;
-import com.olva.sunatfe.be.UBLExtensionsType;
-import java.math.BigDecimal;
+import com.olva.sunatfe.util.MyPrefixMapper;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.transform.dom.DOMResult;
+import javax.xml.bind.PropertyException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  *
@@ -61,40 +51,42 @@ public class FacturaElectronicaTest {
     public void marshallFacturaElectronica() {
         try {
             ObjectFactory ob = new ObjectFactory();
-            IDType idAMonetaryTotal = ob.createIDType();
-            idAMonetaryTotal.setValue("1001");
-            PayableAmountType pa = ob.createPayableAmountType();
-            pa.setValue(new BigDecimal("348199.15"));
-            AdditionalMonetaryTotalType amtt = ob.createAdditionalMonetaryTotalType();
-            amtt.setID(idAMonetaryTotal);
-            amtt.setPayableAmount(pa);
-            IDType idAMonetaryTotal2 = ob.createIDType();
-            idAMonetaryTotal2.setValue("1003");
-            AdditionalMonetaryTotalType amtt2 = ob.createAdditionalMonetaryTotalType();
-            PayableAmountType pa2 = ob.createPayableAmountType();
-            pa2.setValue(new BigDecimal("12350.00"));
-            amtt2.setID(idAMonetaryTotal2);
-            amtt2.setPayableAmount(pa2);
-            AdditionalInformationTypeSunatAgg aits = ob.createAdditionalInformationTypeSunatAgg();
-            aits.getAdditionalMonetaryTotal().add(amtt);
-            aits.getAdditionalMonetaryTotal().add(amtt2);
-            ExtensionContentType ect = ob.createExtensionContentType();
-            JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
-            DOMResult res = new DOMResult();
-            context.createMarshaller().marshal(aits, res);
-            Element elt = ((Document) res.getNode()).getDocumentElement();
-            Element elem = elt;
-            ect.setAny(elem);
-            UBLExtensionType uet = ob.createUBLExtensionType();
-            uet.setExtensionContent(ect);
-            UBLExtensionsType uest = ob.createUBLExtensionsType();
-            uest.getUBLExtension().add(uet);
-            InvoiceType invoiceType = ob.createInvoiceType();
-            invoiceType.setUBLExtensions(uest);
+//            IDType idAMonetaryTotal = ob.createIDType();
+//            idAMonetaryTotal.setValue("1001");
+//            PayableAmountType pa = ob.createPayableAmountType();
+//            pa.setValue(new BigDecimal("348199.15"));
+//            AdditionalMonetaryTotalType amtt = ob.createAdditionalMonetaryTotalType();
+//            amtt.setID(idAMonetaryTotal);
+//            amtt.setPayableAmount(pa);
+//            IDType idAMonetaryTotal2 = ob.createIDType();
+//            idAMonetaryTotal2.setValue("1003");
+//            AdditionalMonetaryTotalType amtt2 = ob.createAdditionalMonetaryTotalType();
+//            PayableAmountType pa2 = ob.createPayableAmountType();
+//            pa2.setValue(new BigDecimal("12350.00"));
+//            amtt2.setID(idAMonetaryTotal2);
+//            amtt2.setPayableAmount(pa2);
+//            AdditionalInformationTypeSunatAgg aits = ob.createAdditionalInformationTypeSunatAgg();
+//            aits.getAdditionalMonetaryTotal().add(amtt);
+//            aits.getAdditionalMonetaryTotal().add(amtt2);
+//            ExtensionContentType ect = ob.createExtensionContentType();
+//            Element elem = (Element) aits;
+//            ect.setAny(elem);
+//            UBLExtensionType uet = ob.createUBLExtensionType();
+//            uet.setExtensionContent(ect);
+//            UBLExtensionsType uest = ob.createUBLExtensionsType();
+//            uest.getUBLExtension().add(uet);
+//            InvoiceType invoiceType = ob.createInvoiceType();
+//            invoiceType.setUBLExtensions(uest);
             JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
             Marshaller marshaller = jc.createMarshaller();
+            MyPrefixMapper m = new MyPrefixMapper();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
             marshaller.setProperty(Marshaller.JAXB_ENCODING, "ISO-8859-1");
+            try {
+            marshaller.setProperty("com.sun.xml.internal.bind.namespacePrefixMapperx", m);
+            }catch(PropertyException e){
+                Logger.getLogger(FacturaElectronicaTest.class.getName()).log(Level.SEVERE, null, e);
+            }
             marshaller.marshal(ob.createInvoice(null), System.out);
         } catch (JAXBException ex) {
             Logger.getLogger(FacturaElectronicaTest.class.getName()).log(Level.SEVERE, null, ex);
