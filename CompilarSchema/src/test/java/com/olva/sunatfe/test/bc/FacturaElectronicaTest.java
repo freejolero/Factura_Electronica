80d0d6efff91048c90d4071b32d2df27c68b61fe
christian <christian@Chris-Pc>
2015-06-04 21:07:30 -0500

    Probando resolucion de prefijos

--- CompilarSchema/src/test/java/com/olva/sunatfe/test/bc/FacturaElectronicaTest.java
+++ CompilarSchema/src/test/java/com/olva/sunatfe/test/bc/FacturaElectronicaTest.java
@@ -12,6 +12,7 @@ import com.olva.sunatfe.enu.CodigoConceptosTributarios;
 import com.olva.sunatfe.enu.CodigoElementosAdicionalesComprobante;
 import java.io.File;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
@@ -60,7 +61,9 @@ public class FacturaElectronicaTest {
             invoice.addFacturaExtensionesExtensionContenidoDeExtensionInformacionAdicionalTotalMonetario(CodigoConceptosTributarios.TOTAL_VALO_VENTA_OPERACIONES_EXONERADAS, new BigDecimal("12350.00"));
             invoice.addFacturaExtensionesExtensionContenidoDeExtensionInformacionAdicionalPropiedadAdicional(CodigoElementosAdicionalesComprobante.MONTO_EN_LETRAS, "CUATROCIENTOS VEINTITRES MIL DOSCIENTOS VEINTICINCO Y 00/100");
             invoice.setUBLIdVersion("2.0");
-            
+            invoice.setCustomizationId("1.0");
+            invoice.setId("F001-4355");
+            invoice.setIssueDate(new Date());
             invoice.generar();
         } catch (JAXBException ex) {
             Logger.getLogger(FacturaElectronicaTest.class.getName()).log(Level.SEVERE, null, ex);
